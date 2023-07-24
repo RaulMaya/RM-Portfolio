4837579d34c71543d069a9f97a0b6a857ad61033
Raul <raulmayas20@gmail.com>
2023-07-24 16:37:57 -0600

    Init Portfolio

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,7 @@
-import "./App.css";
+import "./input.css"
 
 function App() {
-  const name = "Raul Maya";
-  const age = 27;
-  return (
-    <div>
-      <h1>Hello there</h1>
-      <h3>General Kenobi</h3>
-      <h5>Hi, my name is {name}, and I'm {age} year old</h5>
-      <input type="number" min={5} max={10} placeholder="7" style={{border: '1px solid blue'}}/>
-      <textarea autoFocus={true} />
-    </div>
-  );
+  return <h1 class="text-4xl font-bold underline">Hello there</h1>;
 }
 
 export default App;
