fedfecf7c61db97c02aabf744b7148cf95ea9835
Raul <raulmayas20@gmail.com>
2023-09-10 22:05:55 -0600

    Filling the about me page

--- client/src/components/Contact/ContactInformation.js
+++ client/src/components/Contact/ContactInformation.js
@@ -13,109 +13,73 @@ import {
   faLocation,
   faPhone,
 } from "@fortawesome/free-solid-svg-icons";
-import "../../socialIcons.css";
+import "./socialIcons.css";
 
 const ContactInformation = () => {
+  const socialApps = [
+    {
+      name: "Facebook",
+      class: "facebook",
+      icon: faFacebook,
+      url: "https://www.facebook.com/raul.maya.507",
+    },
+    {
+      name: "Twitter",
+      class: "twitter",
+      icon: faXTwitter,
+      url: "https://twitter.com/RaulMaya11",
+    },
+    {
+      name: "Instagram",
+      class: "instagram",
+      icon: faInstagram,
+      url: "https://www.instagram.com/raul.mayas/",
+    },
+    {
+      name: "Github",
+      class: "github",
+      icon: faGithub,
+      url: "https://github.com/RaulMaya",
+    },
+    {
+      name: "Linkedin",
+      class: "linkedin",
+      icon: faLinkedin,
+      url: "https://www.linkedin.com/in/raul-maya/",
+    },
+    { name: "Whatsapp", class: "whatsapp", icon: faWhatsapp, url: "url" },
+    { name: "Email", class: "email", icon: faEnvelope, url: "url" },
+    {
+      name: "Location",
+      class: "location",
+      icon: faLocation,
+      url: "https://goo.gl/maps/2oNiqjMmMmiaWfb37",
+    },
+    {
+      name: "Phone Call",
+      class: "phone",
+      icon: faPhone,
+      url: "tel:+52833-159-7006",
+    },
+  ];
   return (
     <div className="basis-1/2 border-l-2 border-black min-h-screen bg-cyan-400 text-center">
       <h5 className="text-center text-5xl">Contact Information</h5>
-      <ul className="mt-7">
-        <section className="flex justify-center">
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full facebook">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Facebook</span>
-            <a
-              className="icon-url"
-              href="https://www.facebook.com/raul.maya.507"
-            >
+      <ul className="mt-7 grid grid-cols-2 md:grid-cols-3 gap-4">
+        {socialApps.map((app) => (
+          <li
+            className={`icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full ${app.class}`}
+          >
+            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">
+              {app.name}
+            </span>
+            <a className="icon-url" href={app.url}>
               <span>
-                <FontAwesomeIcon icon={faFacebook} />
+                <FontAwesomeIcon icon={app.icon} />
               </span>
             </a>
           </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full twitter">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Twitter</span>
-            <a className="icon-url" href="https://twitter.com/RaulMaya11">
-              <span>
-                <FontAwesomeIcon icon={faXTwitter} />
-              </span>
-            </a>
-          </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full instagram">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Instagram</span>
-            <a
-              className="icon-url"
-              href="https://www.instagram.com/raul.mayas/"
-            >
-              <span>
-                <FontAwesomeIcon icon={faInstagram} />
-              </span>
-            </a>
-          </li>
-        </section>
-        <section className="flex justify-center">
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full github">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Github</span>
-            <a className="icon-url" href="https://github.com/RaulMaya">
-              <span>
-                <FontAwesomeIcon icon={faGithub} />
-              </span>
-            </a>
-          </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full linkedin">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">LinkedIn</span>
-            <a
-              className="icon-url"
-              href="https://www.linkedin.com/in/raul-maya/"
-            >
-              <span>
-                <FontAwesomeIcon icon={faLinkedin} />
-              </span>
-            </a>
-          </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full whatsapp">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">WhatsApp</span>
-            <a
-              className="icon-url"
-              href="https://www.linkedin.com/in/raul-maya/"
-            >
-              <span>
-                <FontAwesomeIcon icon={faWhatsapp} />
-              </span>
-            </a>
-          </li>
-        </section>
-        <section className="flex justify-center">
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full email">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Email</span>
-            <a
-              className="icon-url"
-              href="https://www.linkedin.com/in/raul-maya/"
-            >
-              <span>
-                <FontAwesomeIcon icon={faEnvelope} />
-              </span>
-            </a>
-          </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full location">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Location</span>
-            <a
-              className="icon-url"
-              href="https://goo.gl/maps/2oNiqjMmMmiaWfb37"
-            >
-              <span>
-                <FontAwesomeIcon icon={faLocation} />
-              </span>
-            </a>
-          </li>
-          <li className="icon relative transition-all flex justify-center w-16 h-16 text-xl flex-col bg-white items-center cursor-pointer shadow-xl relative p-7 m-7 rounded-full phone">
-            <span className="tooltip absolute top-0 text-sm bg-white text-white px-2 py-1.5 rounded-md shadow-xl opacity-0 pointer-events-none transition-all whitespace-nowrap">Phone Call</span>
-            <a className="icon-url" href="tel:+52833-159-7006">
-              <span>
-                <FontAwesomeIcon icon={faPhone} />
-              </span>
-            </a>
-          </li>
-        </section>
+        ))}
       </ul>
     </div>
   );
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,13 +1,37 @@
 import { React, useEffect } from "react";
 import NavBarComponent from "../components/NavBarComponent";
+import raulImage from "../raulImage.jpg";
 
 const About = () => {
-  useEffect(()=> {
-    document.title ="About Me"
-  },[])
+  useEffect(() => {
+    document.title = "About Me";
+  }, []);
   return (
     <>
       <h1 className="text-center text-10xl">ABOUT ME</h1>
+      <section className="flex flex-row justify-center">
+        <article className="basis-1/4 flex justify-center">
+          <img
+            className="object-cover w-56 h-56"
+            src={raulImage}
+            alt="Raul Maya in the americas tower"
+          />
+        </article>
+        <article className="basis-3/4">
+          <h3>Introduction</h3>
+          <p>Raul Maya Salazar</p>
+          <p>
+            Web Developer and Data Analytics Engineer with a background in
+            Industrial Engineering and life-long dedication to learning.
+            Certified by Tecnológico de Monterrey Data Analytics & Visualization
+            Bootcamp, and by Tecnológico de Monterrey Web Development Bootcamp.
+            Effective at combining creativity and problem solving to make sense
+            of your information and build easy-to-use applications based on it.
+            Recognized among employees for his sharp mind and attention to
+            detail, no matter how complex the project.
+          </p>
+        </article>
+      </section>
       <NavBarComponent />
     </>
   );
