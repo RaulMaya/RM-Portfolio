064108f706eb7a49f90a24a72ace9dc1630b5239
raulmaya <raulmayas20@gmail.com>
2024-05-13 12:50:33 -0600

    Add Heroku post-build script and correct server configuration

--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")))
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
 }
 
 const startApolloServer = async () => {
